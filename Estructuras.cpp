eae24f9c4ab6b70aaccf85943c914c603d0b6fd4
USUARIO <USUARIO@172.24.42.22>
2016-09-09 13:12:29 -0600

    Por fin punco

--- Estructuras.cpp
+++ Estructuras.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
     cout<<"  Proyecto de Estructuras de Datos\n   Estudiantes:\n\t-Eliomar Rodriguez\n\t-Steven Peraza"<<endl;
     cout << "Funciona pedazo de mierda!!!";
+    cout <<"Peeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeerrrrrrrrrrrrrraaaaaaaaaaaaaaaaaaaa por fin puto!";
     return 0;
 }
 

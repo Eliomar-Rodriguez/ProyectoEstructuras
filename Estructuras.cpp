53a92b684f102f730c5d5be6c5c94dae0d9d3361
Steven-Peraza <perazas376@gmail.com>
2016-09-09 22:18:36 -0600

    Avances en la creación de las listas de Examenes, Preguntas de Marque con X y Preguntas de Respuesta Corta

--- Estructuras.cpp
+++ Estructuras.cpp
@@ -11,10 +11,12 @@ struct Examen
     string profe;
     int total_puntos,correctas,malas,nota;
     // poner las referencias hacia las listas de los dos tipos de preguntas
-};
+    struct Examen * sig;
+}*cabezaExamen;
 
-// preugntas de marque con x
-struct nodoMarqX 
+
+// preguntas de marque con x
+struct MarqX 
 {
     /* numPreg  ->  numero de pregunta
      * resp     ->  respuesta correcta de la pregunta
@@ -26,15 +28,16 @@ struct nodoMarqX
     string numPreg,resp,respEst,tipo,nombSec;
     string opciones[];
     int valor;
-    struct nodoMarqX* sig;
+    struct MarqX* sig;
     
-}*cabezaMarqX;
+}*cabezaX;
 //\xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
 //preguntas de respuesta breve
-struct nodoRespCort
+struct RespCort
 {
     /* numPreg  ->  numero de pregunta
+     * pregunta ->  la pregunta en si
      * resp     ->  respuesta correcta de la pregunta
      * respEst  ->  respuesta ingresada por el estudiante
      * tipo     ->  tipo de pregunta  ( marque x  ,  respuesta breve )
@@ -42,18 +45,108 @@ struct nodoRespCort
      * valor    ->  cantidad de puntos que va a valer esa pregunta
      * porcAcierto -> porcentaje de igualdad que se acepta para dar por buena una respuesta
      */
-    string numPreg,resp,respEst,tipo,nomSec;
+    string numPreg,resp,respEst,tipo,nomSec,pregunta;
     int porcAcierto = 60;
     int valor;
-    struct nodoRespCort* sig;
+    struct RespCort* sig;
     
-}*cabezaRespCort;
+}*cabezaRC;
 
 //\xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-int main() 
+
+void insertarExamenes()
 {
-    cout<<"Aqui se llaman las varas";
-    return 0;
+    //solicitar memoria
+    struct Examen* nn;
+    nn = new struct Examen; 
+    string nom;
+    string pro;
+    
+    cout << "Digite el nombre del Examen" << endl;
+    getline(cin,nom);
+    cout << "Digite el nombre del profe" << endl;
+    getline(cin,pro);
+    
+    //lleno datos
+    nn->nombre = nom;
+    nn->profe  = pro;
+    nn->total_puntos = 0;
+    nn->correctas  = 0;
+    nn->malas  = 0;
+    nn->nota  = 0;
+    nn->sig = NULL;
+
+    //enlazar
+    if (cabezaExamen == NULL)
+        cabezaExamen = nn;
+    else
+    {
+        nn->sig = cabezaExamen;
+        cabezaExamen = nn;
+    }
 }
 
+void imprimirListaExamenes()
+{
+    struct Examen* temp = cabezaExamen;
+
+    while (temp != NULL)
+    {
+        cout << temp->nombre << endl;
+        temp = temp->sig;
+    }
+}
+
+void insertarPreguntasCortas()
+{
+    //solicitar memoria
+    struct RespCort* nn;
+    nn = new struct RespCort; 
+    string pre;
+    string res;
+
+    cout << "Escriba la pregunta." << endl;
+    getline(cin,pre);
+    cout << "Escriba la respuesta de la pregunta." << endl;
+    getline(cin,res);
+    
+    //lleno datos
+    nn->pregunta = pre;
+    nn->resp = res;
+    nn->respEst  = "";
+    nn->tipo = "Corta";
+    nn->nomSec = "";
+    nn->porcAcierto = 0;
+    nn->valor = 0;
+    nn->sig = NULL;
+
+    //enlazar
+    if (cabezaRC == NULL)
+        cabezaRC = nn;
+    else
+    {
+        nn->sig = cabezaRC;
+        cabezaRC = nn;
+    }
+}
+
+void imprimirListaPreguntasRC()
+{
+    struct RespCort* temp = cabezaRC;
+
+    while (temp != NULL)
+    {
+        cout << temp->pregunta << endl;
+        cout << temp->resp << endl;
+        temp = temp->sig;
+    }
+}
+int main() 
+{
+    //insertarExamenes();
+    //imprimirListaExamenes();
+    //insertarPreguntasCortas();
+    //imprimirListaPreguntasRC();
+    return 0;
+}
